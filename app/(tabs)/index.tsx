e07bcdf8e2d146c3366bbd387efc9a09be767cd2
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-02-02 19:58:54 -0300

    Native paper copnfigurado

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const [text, setText] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
---
+++ components/Account/RegisterForm/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TextInput } from 'react-native-paper';
+
+const RegisterForm = () => {};
+
+export default RegisterForm;
