9c712089d1f1602fab8bd006e63ccc7a026a3e1c
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-02-03 12:53:25 -0300

    Componente de formulario de registro adicionado com formik e yup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import RegisterForm from '@/components/Account/RegisterForm';
 
 export default function TabOneScreen() {
-  const [text, setText] = React.useState("");
+  const [text, setText] = React.useState('');
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <RegisterForm />
     </View>
   );
 }
--- components/Account/RegisterForm/index.tsx
+++ components/Account/RegisterForm/index.tsx
@@ -1,6 +1,74 @@
+import { View } from '@/components/Themed';
+import UserPropsInterface from '@/interfaces/UserPropsInterface';
+import RegisterValidation from '@/validations/RegisterValidation';
+import { Formik } from 'formik';
 import React from 'react';
-import { TextInput } from 'react-native-paper';
 
-const RegisterForm = () => {};
+import { Button, TextInput, Text } from 'react-native-paper';
+
+const RegisterForm = () => {
+	const handleRegister = async (values: UserPropsInterface) => {
+		console.log(values);
+	};
+
+  return (
+    <View>
+      <>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+          }}
+          validationSchema={RegisterValidation}
+          onSubmit={(values) => handleRegister(values)}
+        >
+          {({ handleChange, handleSubmit, values, errors }) => (
+            <View>
+              <TextInput
+                label="Nome"
+                placeholder="Seu nome"
+                value={values.name}
+                onChangeText={handleChange('name')}
+              />
+              {errors.name && <Text>{errors.name}</Text>}
+
+              <TextInput
+                label="Email"
+                placeholder="Seu email"
+                value={values.email}
+                onChangeText={handleChange('email')}
+              />
+              {errors.email && <Text>{errors.email}</Text>}
+
+              <TextInput
+                label="Senha"
+                placeholder="Digite sua senha"
+                value={values.password}
+                secureTextEntry={true}
+                onChangeText={handleChange('password')}
+              />
+              {errors.password && <Text>{errors.password}</Text>}
+
+              <TextInput
+                label="Senha"
+                placeholder="Digite sua senha"
+                value={values.confirmPassword}
+                secureTextEntry={true}
+                onChangeText={handleChange('confirmPassword')}
+              />
+              {errors.confirmPassword && <Text>{errors.confirmPassword}</Text>}
+
+              <Button onPress={(e: any) => handleSubmit(e)}>
+								Criar 
+							</Button>
+            </View>
+          )}
+        </Formik>
+      </>
+    </View>
+  );
+};
 
 export default RegisterForm;
