e4319bcfabd78d22c2b55c5fda9688ce86adb087
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-02-12 18:16:15 -0300

    Componente de login finalizado

--- components/Account/LoginForm/index.tsx
+++ components/Account/LoginForm/index.tsx
@@ -4,8 +4,7 @@ import { Formik } from 'formik';
 import { Button, Text, TextInput } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import LoginValidation from '@/validations/LoginValidation';
-import { Entypo } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
 
 const LoginForm = () => {
   return (
@@ -43,19 +42,26 @@ const LoginForm = () => {
               <Button style={styles.space} onPress={(e: any) => handleSubmit(e)} mode={'contained'}>
                 Login
               </Button>
+
+              <Text variant={'titleMedium'} style={{ textDecorationLine: 'line-through', textAlign: 'center' }}>
+                Esqueceu sua senha?
+              </Text>
             </View>
           )}
         </Formik>
       </>
       <View style={styles.optionsView}>
-        <Button style={[styles.space, styles.buttonFacebook]} onPress={(e: any) => console.log(e)} mode={'contained'}>
-          <Entypo name="facebook-with-circle" size={24} color="black" />
-          Login
-        </Button>
-        <Button style={[styles.space, styles.buttonGoogle]} onPress={(e: any) => console.log(e)} mode={'contained'}>
-          <FontAwesome name="google-plus-circle" size={24} color="black" />
-          Login
-        </Button>
+        <Text variant={'titleMedium'}>
+          Ou faça login utilizando suas redes sociais:
+        </Text>
+        <View style={styles.buttonsView}>
+          <Button style={[styles.space, styles.buttonFacebook, styles.circularButton]} onPress={(e: any) => console.log(e)} mode={'contained'}>
+            <FontAwesome5 name="facebook" size={20} color="black" />
+          </Button>
+          <Button style={[styles.space, styles.buttonGoogle, styles.circularButton]} onPress={(e: any) => console.log(e)} mode={'contained'}>
+            <FontAwesome name="google" size={20} color="black" />
+          </Button>
+        </View>
       </View>
     </View>
   );
@@ -86,17 +92,24 @@ const styles = StyleSheet.create({
   },
   optionsView: {
     marginTop: 40,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonFacebook: {
-    alignItems: 'center',
     backgroundColor: '#3b5998',
-    width: '60%'
   },
   buttonGoogle: {
-    alignItems: 'center',
+    marginLeft: 10,
     backgroundColor: '#db4a39',
-    width: '60%'
+  },
+  circularButton: {
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonsView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
 });
 
