607319c785531acf6554dfb94f39016449fc95dc
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-02-20 00:22:30 -0300

    Botao flutuante para cadastro de solicitacoes adicionado

--- components/Shared/Navbar/index.tsx
+++ components/Shared/Navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Appbar } from 'react-native-paper';
+import { Entypo } from '@expo/vector-icons';
 
 const Navbar = () => {
   return (
     <Appbar.Header style={{height: 50}}>
       <Appbar.Content title="Agora" />
+      <Entypo name="megaphone" size={24} color="black" />
     </Appbar.Header>
   );
 }
