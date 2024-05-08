fddbd9ac1980ec6eca0eb7efeca89bb56e5ad082
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-05-08 15:42:34 -0300

    NPM substituido por Yarn e mapa adicionado

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import { View } from '@/src/components/Themed';
 import ContainerBaseStyle from '@/app/style';
 import Navbar from '@/src/components/Shared/Navbar';
+import AgoraMap from '@/src/components/Map/AgoraMap';
 
 export default function TabOneScreen() {
 
   return (
     <>
-      <Navbar />
-      <View style={ContainerBaseStyle.container}>
+      {/*<Navbar />*/}
+      <AgoraMap/>
+      {/*<View style={ContainerBaseStyle.container}>*/}
 
-      </View>
+      {/*</View>*/}
     </>
   );
 }
