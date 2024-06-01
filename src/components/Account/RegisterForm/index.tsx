85d77173f4cfc5ae0e94367adab73cf61386ebd1
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-05-31 23:26:22 -0300

    Errors tratados

--- src/components/Account/RegisterForm/index.tsx
+++ src/components/Account/RegisterForm/index.tsx
@@ -3,24 +3,24 @@ import RegisterValidation from '@/src/validations/RegisterValidation';
 import { Formik } from 'formik';
 import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-
 import { Button, TextInput, Text } from 'react-native-paper';
 import { register } from '@/src/services/api/AuthService';
 import { View } from '@/src/components/Themed';
 import PasswordInput from '@/src/components/Account/PasswordInput';
 import { useRouter } from 'expo-router';
 import { errorToast, successToast } from '@/utils/use-toast';
+import FormErrorsInterface from '@/src/interfaces/Forms/FormErrorsInterface';
 
 const RegisterForm = () => {
   const router = useRouter();
-  const [formErrors, setFormErrors] = React.useState<string[]>([]);
+  const [formErrors, setFormErrors] = React.useState<FormErrorsInterface>({});
 
   useEffect(() => {
     console.log(formErrors);
   }, [formErrors]);
 
   const cleanErrors = () => {
-    setFormErrors([]);
+    setFormErrors({});
   }
 
   const handleRegister = async (values: UserPropsInterface) => {
@@ -31,7 +31,7 @@ const RegisterForm = () => {
       })
       .catch((error) => {
         if (error?.response?.status === 422) {
-          setFormErrors(error?.response?.data?.errors || []);
+          setFormErrors(error?.response?.data?.errors || {});
           errorToast({ title: 'Ocorreu um erro ao tentar registrar seu usuário!' });
         } else {
           errorToast({ title: 'Ocorreu um erro ao tentar registrar seu usuário!' });
@@ -41,61 +41,62 @@ const RegisterForm = () => {
 
   return (
     <View style={styles.container}>
-      <>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-            password_confirmation: '',
-          }}
-          validationSchema={RegisterValidation}
-          onSubmit={(values) => handleRegister(values)}
-        >
-          {({ handleChange, handleSubmit, values, errors }) => (
-            <View style={styles.form}>
-              <TextInput
-                style={styles.space}
-                label="Nome"
-                placeholder="Seu nome"
-                value={values.name}
-                onChangeText={handleChange('name')}
-              />
-              {errors.name && <Text>{errors.name}</Text>}
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+          password_confirmation: '',
+        }}
+        validationSchema={RegisterValidation}
+        onSubmit={(values) => handleRegister(values)}
+      >
+        {({ handleChange, handleSubmit, values, errors, touched }) => (
+          <View style={styles.form}>
+            <TextInput
+              style={styles.space}
+              label="Nome"
+              placeholder="Seu nome"
+              value={values.name}
+              onChangeText={handleChange('name')}
+            />
+            {(errors.name && touched.name) && <Text id="error-name">{errors.name}</Text>}
+            {formErrors.name && formErrors.name.length > 0 && <Text id="form-error-name">{formErrors.name[0]}</Text>}
 
-              <TextInput
-                style={styles.space}
-                label="Email"
-                placeholder="Seu email"
-                value={values.email}
-                onChangeText={handleChange('email')}
-              />
-              {errors.email && <Text>{errors.email}</Text>}
+            <TextInput
+              style={styles.space}
+              label="Email"
+              placeholder="Seu email"
+              value={values.email}
+              onChangeText={handleChange('email')}
+            />
+            {(errors.email && touched.email) && <Text id="error-email">{errors.email}</Text>}
+            {formErrors.email && formErrors.email.length > 0 && <Text id="form-error-email">{formErrors.email[0]}</Text>}
 
-              <PasswordInput
-                label={'Senha'}
-                value={values.password}
-                placeholder={'Sua senha'}
-                onChangeText={handleChange('password')}
-              />
-              {errors.password && <Text>{errors.password}</Text>}
+            <PasswordInput
+              label={'Senha'}
+              value={values.password}
+              placeholder={'Sua senha'}
+              onChangeText={handleChange('password')}
+            />
+            {(errors.password && touched.password) && <Text id="error-password">{errors.password}</Text>}
+            {formErrors.password && formErrors.password.length > 0 && <Text id="form-error-password">{formErrors.password[0]}</Text>}
 
+            <PasswordInput
+              label="Confirmação de Senha"
+              value={values.password_confirmation}
+              placeholder="Digite a confirmação da senha"
+              onChangeText={handleChange('password_confirmation')}
+            />
+            {(errors.password_confirmation && touched.password_confirmation) && <Text id="error-password_confirmation">{errors.password_confirmation}</Text>}
+            {formErrors.password_confirmation && formErrors.password_confirmation.length > 0 && <Text id="form-error-password_confirmation">{formErrors.password_confirmation[0]}</Text>}
 
-              <PasswordInput
-                label="Confirmação de Senha"
-                value={values.password_confirmation}
-                placeholder="Digite a confirmação da senha"
-                onChangeText={handleChange('password_confirmation')}
-              />
-              {errors.password_confirmation && <Text>{errors.password_confirmation}</Text>}
-
-              <Button style={styles.space} onPress={(e: any) => handleSubmit(e)} mode={'contained'}>
-                Criar
-              </Button>
-            </View>
-          )}
-        </Formik>
-      </>
+            <Button style={styles.space} onPress={(e: any) => handleSubmit(e)} mode={'contained'}>
+              Criar
+            </Button>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
@@ -115,13 +116,9 @@ const styles = StyleSheet.create({
   form: {
     width: '80%',
   },
-  logoImage: {
-    width: 350,
-    height: 200,
-  },
-  text: {
-    textAlign: 'center',
-    color: 'white',
+  errorText: {
+    color: 'red',
+    marginTop: 5,
   },
 });
 
