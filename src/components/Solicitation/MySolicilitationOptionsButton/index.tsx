b5376da15b4d114d4f7971f43a52e132fa33e2c5
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2024-09-05 20:25:51 -0300

    Botao de opcoes para solicitacao adicionado

---
+++ src/components/Solicitation/MySolicilitationOptionsButton/index.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Animated } from 'react-native';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
+import PaginatedSolicitationInterface from '@/src/interfaces/Solicitation/PaginatedSolicitationInterface';
+
+const RADIUS = 60; // Aumenta o raio para acomodar os botões de forma mais espaçosa
+const BUTTON_SIZE = 45; // Aumenta o tamanho dos botões para melhor usabilidade
+
+const MySolicitationOptionsButton = ({ solicitationData }: { solicitationData: PaginatedSolicitationInterface }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const animatedValue = useState(new Animated.Value(0))[0]; // Valor animado para a transição suave
+
+  const handleButtonPress = () => {
+    setIsOpen(!isOpen);
+
+    Animated.spring(animatedValue, {
+      toValue: isOpen ? 0 : 1,
+      friction: 6,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const options = [
+    {
+      icon: <Feather name="eye" size={24} color="black" />,
+      action: () => console.log(`Visualizar Solicitação: ${solicitationData.title}`)
+    },
+    {
+      icon: <Feather name="edit" size={24} color="black" />,
+      action: () => console.log(`Editar Solicitação: ${solicitationData.title}`)
+    },
+    {
+      icon: <MaterialIcons name="published-with-changes" size={24} color="black" />,
+      action: () => console.log(`Atualizar Status da Solicitação: ${solicitationData.title}`)
+    },
+    {
+      icon: <Feather name="trash" size={24} color="black" />,
+      action: () => console.log(`Excluir Solicitação: ${solicitationData.title}`)
+    },
+  ];
+
+  return (
+    <View style={styles.floatContainer}>
+      {options.map((option, index) => {
+        const angle = (index * (180 / (options.length - 1))) * (Math.PI / 180); // Ângulo semicircular
+        const x = RADIUS * Math.cos(angle);
+        const y = RADIUS * Math.sin(angle);
+
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.subButtonContainer,
+              {
+                transform: [
+                  { translateX: animatedValue.interpolate({ inputRange: [0, 1], outputRange: [0, x] }) },
+                  { translateY: animatedValue.interpolate({ inputRange: [0, 1], outputRange: [0, -y] }) }
+                ],
+                opacity: animatedValue,
+              }
+            ]}
+          >
+            <TouchableOpacity
+              style={styles.subButton}
+              onPress={option.action}
+            >
+              {option.icon}
+            </TouchableOpacity>
+          </Animated.View>
+        );
+      })}
+
+      <TouchableOpacity
+        onPress={handleButtonPress}
+        activeOpacity={0.7}
+        style={styles.button}
+      >
+        <Feather name={isOpen ? 'x' : 'menu'} size={28} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  floatContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    zIndex: 1000,
+  },
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    backgroundColor: '#004aad',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 6,
+    zIndex: 1001,
+  },
+  subButtonContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'visible',
+    zIndex: 1000,
+  },
+  subButton: {
+    backgroundColor: '#f0f0f0',
+    borderRadius: BUTTON_SIZE / 2,
+    width: BUTTON_SIZE,
+    height: BUTTON_SIZE,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    elevation: 6,
+  },
+});
+
+export default MySolicitationOptionsButton;
--- src/components/Solicitation/SolicitationCard/index.tsx
+++ src/components/Solicitation/SolicitationCard/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Text } from 'react-native-paper';
-import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, View } from 'react-native';
 import PaginatedSolicitationInterface from '@/src/interfaces/Solicitation/PaginatedSolicitationInterface';
+import MySolicitationOptionsButton from '@/src/components/Solicitation/MySolicilitationOptionsButton';
 
 const SolicitationCard = ({ solicitationData }: { solicitationData: PaginatedSolicitationInterface }) => {
+  const [isOptionsVisible, setIsOptionsVisible] = useState(false);
+
+  const openOptions = () => setIsOptionsVisible(true);
+  const closeOptions = () => setIsOptionsVisible(false);
+
   return (
     <View style={styles.cardSpace}>
       <Card style={styles.cardLayout}>
         <Card.Title titleVariant="titleLarge" title={solicitationData.title} style={styles.title} />
 
-        <View style={styles.imageContainer}>
+        <View style={styles.likesContainer}>
+          <View style={styles.likeIcon}>
+            <MaterialIcons name="thumb-up" size={24} color="white" />
+          </View>
+          <Text style={styles.likesText}>{solicitationData.likesCount}</Text>
+        </View>
 
+        <View style={styles.imageContainer}>
           {solicitationData.coverImage ? (
             <Card.Cover source={{ uri: solicitationData.coverImage }} style={styles.cardCover} />
           ) : (
@@ -19,23 +31,10 @@ const SolicitationCard = ({ solicitationData }: { solicitationData: PaginatedSol
               <MaterialCommunityIcons name="image-marker-outline" size={150} color="black" />
             </View>
           )}
-
-          <View style={styles.likesContainer}>
-            <View style={styles.likeIcon}>
-              <MaterialIcons name="thumb-up" size={24} color="white" />
-            </View>
-            <Text style={styles.likesText}>{solicitationData.likesCount}</Text>
-          </View>
         </View>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.circularButton} onPress={() => console.log('Edit button pressed')}>
-            <Feather name="edit" size={25} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.circularButton}
-                            onPress={() => console.log('Published with changes button pressed')}>
-            <MaterialIcons name="published-with-changes" size={25} color="white" />
-          </TouchableOpacity>
+          <MySolicitationOptionsButton solicitationData={solicitationData} />
         </View>
       </Card>
     </View>
@@ -51,7 +50,7 @@ const styles = StyleSheet.create({
   cardLayout: {
     width: '80%',
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: 'visible',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
@@ -70,7 +69,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: 5,
     position: 'absolute',
-    top: 10,
+    top: 5,
     right: 10,
     borderRadius: 50,
   },
@@ -85,6 +84,7 @@ const styles = StyleSheet.create({
   likesText: {
     color: 'white',
     marginLeft: 5,
+    marginBottom: 5,
     fontSize: 12,
   },
   iconContainer: {
@@ -98,20 +98,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   actions: {
+    overflow: 'visible',
     position: 'absolute',
     bottom: 10,
     right: 10,
     flexDirection: 'row',
     marginTop: 10,
-  },
-  circularButton: {
-    borderRadius: 30,
-    width: 45,
-    height: 45,
-    backgroundColor: '#004aad',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 10,
+    zIndex: 1000,
   },
 });
 
